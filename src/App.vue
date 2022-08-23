647cf91f87914375dad52f84fb5bd41c89d19153
alirezaahmadi1308 <alirezaahmadi1308@gmail.com>
2022-08-23 21:41:53 +0430

    create project

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+<div class="container">
+<Alert>
+  <template #title="aa">
+      <h2>hello </h2>
+  <span>{{aa.items}}</span>
+
+</template>
+  <template #text>
+
+<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Obcaecati cumque repellat ipsum commodi, tenetur cupiditate? Itaque provident sed incidunt, eveniet sint natus eos at minus doloremque atque. Atque, tempore saepe?  </p>
+
+</template>
+</Alert>
+
+</div>
+
+</template>
+
+
+
+<script>
+import Alert from "./components/alert.vue";
+
+export default{
+  components:{
+    Alert
+  }
+}
+
+
+</script>
+
+
+<style lang="scss">
+
+</style>
---
+++ src/components/alert.vue
@@ -0,0 +1,40 @@
+<template>
+<div>
+<div class="alert alert-success" role="alert">
+<div v-if="$slots.title">
+<slot name="title" :items="title">
+</slot>
+    <span>
+        title
+        24464664
+    </span>
+</div>
+<slot name="text">  
+</slot>
+  <hr>
+  <p class="mb-0">Whenever you need to, be sure to use margin utilities to keep things nice and tidy.</p>
+</div>
+    
+</div>
+</template>
+
+
+<script >
+export default{
+
+    data:()=>({
+     title:'hahahahaha'
+    }),
+
+    mounted(){
+        this.$slots
+    }
+    }
+
+</script>
+
+
+<style lang="scss"> 
+
+
+</style>
